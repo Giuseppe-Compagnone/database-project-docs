4a5ec4128a03e225a7229773bd13d80d20969894
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-02-25 22:03:17 +0100

    events

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -7,6 +7,7 @@ const Logo = () => {
   const router = useRouter();
 
   const [flag, setFlag] = useState<ChildNode | null>(null);
+  const [input, setInput] = useState<ChildNode | null>(null);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -28,12 +29,44 @@ const Logo = () => {
   }, []);
 
   useEffect(() => {
+    const timer = setInterval(() => {
+      console.log("TIMER");
+      if (!flag) {
+        const item = document.querySelector(
+          "input.nx-block.nx-w-full.nx-appearance-none.nx-rounded-lg.nx-px-3.nx-py-2.nx-transition-colors.nx-text-base.nx-leading-tight"
+        );
+        if (item) {
+          setInput(item);
+          clearInterval(timer);
+        }
+      }
+    }, 500);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  useEffect(() => {
     if (flag) {
       setFlag((old) => {
         // @ts-ignore
         old.firstChild.outerHTML = `<img class="flag" src="https://flagsapi.com/${
           router.locale === "it" ? "IT" : ""
         }${router.locale === "en" ? "GB" : ""}/flat/64.png" />`;
+
+        return old;
+      });
+    }
+  }, [router.locale, flag]);
+
+  useEffect(() => {
+    if (flag) {
+      setInput((old) => {
+        // @ts-ignore
+        old.placeholder = `${
+          router.locale === "it" ? "Cerca nella documentazione..." : ""
+        }${router.locale === "en" ? "Search the docs..." : ""}`;
 
         return old;
       });
